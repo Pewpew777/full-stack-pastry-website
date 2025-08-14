46db4eebb1c17141faaba7da2c6f0c708ccc9d87
Loubna <loubna.yesref15@gmail.com>
2025-08-14 21:46:38 +0200

    add tailwind css cdn link directly

--- resources/views/components/adminlayout.blade.php
+++ resources/views/components/adminlayout.blade.php
@@ -6,6 +6,7 @@
     <title>MarBakes</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<script src="https://cdn.tailwindcss.com"></script>
 <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
