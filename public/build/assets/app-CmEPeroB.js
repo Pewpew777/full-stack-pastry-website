64a669f63cc0792c84bbc5ce38e118400ea5946f
Loubna <loubna.yesref15@gmail.com>
2025-08-14 21:32:50 +0200

    removed build

--- public/build/assets/app-CmEPeroB.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const t=document.getElementById("menu-btn"),e=document.getElementById("mobile-menu");t.addEventListener("click",()=>{e.classList.toggle("hidden"),e.classList.toggle("flex")})});
